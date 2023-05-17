36fc30a01c86520e003292a31049b64d6e288e7a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-05-17 19:57:13 +0530

    c

---
+++ Part-2/ES6/Callback/callbk3.js
@@ -0,0 +1,23 @@
+
+                                let name = "Annie";
+                                // let age = 26; 
+                                let age;
+                                // let name;   
+                                console.log(age);                   
+                                console.log(name);                   
+                                if (name == null || age == null )
+                                {
+                                    console.log("Failure");
+                                    // alert("Details saved successfully");
+                                    // document.getElementById('s').innerHTML = "Details not saved";
+                                }
+                                else
+                                {
+                                    // alert("Please provide the details!");
+                                    // document.getElementById('s').innerHTML = "Details saved";
+                                    console.log("Success");
+
+                                }
+
+                   
+                      
